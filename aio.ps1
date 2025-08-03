32a417d44c4898988dcf2b60ba99b870115bf256
Gelaxiz <130848366+Gelaxiz@users.noreply.github.com>
2025-08-03 22:15:50 +0300

    Create aio.ps1
    the new one

---
+++ aio.ps1
@@ -0,0 +1,9 @@
+mkdir "$env:TEMP\Aioappinstaller"
+cd $env:TEMP\Aioappinstaller
+Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/raw/refs/heads/main/v2/AioInstaller.exe" -Destination "$env:TEMP\Aioappinstaller\Aioinstaller.exe"
+Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/raw/refs/heads/main/v2/msvcp140d.dll" -Destination "$env:TEMP\Aioappinstaller\msvcp140d.dll"
+Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/raw/refs/heads/main/v2/ucrtbased.dll" -Destination "$env:TEMP\Aioappinstaller\ucrtbased.dll"
+Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/raw/refs/heads/main/v2/vcruntime140_1d.dll" -Destination "$env:TEMP\Aioappinstaller\vcruntime140_1d.dll"
+Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/raw/refs/heads/main/v2/vcruntime140d.dll" -Destination "$env:TEMP\Aioappinstaller\vcruntime140d.dll"
+Start-Process Aioinstaller.exe
+exit
