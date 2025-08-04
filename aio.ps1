5ae627c3bf541cb554439e34296d5bad9eaa84a2
Gelaxiz <130848366+Gelaxiz@users.noreply.github.com>
2025-08-04 11:08:42 +0300

    Update aio.ps1

--- aio.ps1
+++ aio.ps1
@@ -5,5 +5,6 @@ Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/raw/refs/heads/m
 Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/raw/refs/heads/main/v2/ucrtbased.dll" -Destination "$env:TEMP\Aioappinstaller\ucrtbased.dll"
 Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/raw/refs/heads/main/v2/vcruntime140_1d.dll" -Destination "$env:TEMP\Aioappinstaller\vcruntime140_1d.dll"
 Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/raw/refs/heads/main/v2/vcruntime140d.dll" -Destination "$env:TEMP\Aioappinstaller\vcruntime140d.dll"
+Start-BitsTransfer -Source "https://github.com/Gelaxiz/aiosetup/raw/refs/heads/main/v2/d3dcompiler_43.dll" -Destination "$env:TEMP\Aioappinstaller\d3dcompiler_43.dll"
 Start-Process Aioinstaller.exe
 exit
